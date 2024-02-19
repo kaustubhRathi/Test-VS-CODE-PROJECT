2f9b3db0fbb766621b32afb166ce1d9e1ce3641a
Shivani Rathi <shivani.rathi@k2partnering.com>
2024-02-19 18:47:40 +0530

    RecapChanges

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ function beforesubmit(){
     console.log('cccc :: ',x.value);
     console.log('cccc :: ',typeof x.value);
 }
+function timestamp() 
+{ var response = document.getElementById("g-recaptcha-response"); 
+if (response == null || response.value.trim() == "") 
+{var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+elems["ts"] = JSON.stringify(new Date().getTime());
+document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+} } setInterval(timestamp, 500);
